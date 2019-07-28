68d28069651237a608d8440d19e1f56931bdfb1d
Idan Kimel <idan.kimel@gmail.com>
2019-07-28 10:00:51 +0300

    Fixed case sensitive typo

--- Timeline.py
+++ Timeline.py
@@ -129,7 +129,7 @@ class Timeline:
 				while event is not None:
 					if event['type'] == TouchScreen.DOWN_EVENT:
 						self.onMouseDown(event['pos'])
-					elif event['type'] == Touchscreen.UP_EVENT:
+					elif event['type'] == TouchScreen.UP_EVENT:
 						self.onMouseUp(event['pos'])
 					event = self.touchScreen.readUpDownEvent()
 
